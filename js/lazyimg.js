4a9b92c944374d9135b4747c98bf36074293f6ab
haoranw <imhaoran.wang@foxmail.com>
2016-10-11 22:28:30 +0800

    Site updated: 2016-10-11 22:28:30

--- js/lazyimg.js
+++ js/lazyimg.js
@@ -1,4 +1,14 @@
 
-
+$(function () {
+    $('img').each(function(index, element) {
+        oldsrc = $(element).attr('src');
+        if (!$(element).attr('lazy-src')){
+            $(element).attr({
+                'src': '',
+                'data-original':  oldsrc
+            });
+        }
+    });
+})
 
 
